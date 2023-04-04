e58ef4eb77cfcc88591f72a6fdb9376489a23b17
ryhoneyman <ryhoneyman@gmail.com>
2023-04-04 05:32:30 +0000

    uplift to base class advanced

--- lib/common/base.class.php
+++ lib/common/base.class.php
@@ -31,10 +31,10 @@
 
 class Base
 {
-   protected $version = 1.0;
-   protected $self    = array();
-   protected $data    = array();
-   protected $debug   = null;
+   protected $version   = 1.0;
+   protected $debug     = null;
+   protected $options   = array();
+   protected $vars      = array();
 
    //===================================================================================================
    // Description: Returns version of this class.
@@ -45,53 +45,52 @@ class Base
 
    //===================================================================================================
    // Description: Creates the class object
-   // Input: object(debug), Debug object created from debug.class
+   // Input: object(debug), Debug object created from debug.class.php
    // Input: array(options), List of options to set in the class
    // Output: null()
    //===================================================================================================
    public function __construct($debug = null, $options = null)
    {
-      $this->debug           = $debug;
-      $this->self['debug']   = $debug;
-      $this->self['options'] = $options;
+      $this->debug = $debug;
 
-      if (isset($options)) {
-         foreach ($options as $option => $optionval) { $this->set_option($option,$optionval); }
-      }
+      if (isset($options)) { $this->options = $options; }
    }
 
    //===================================================================================================
-   // Description: Sets option to class
-   // Input: string(opt), Name of option to set
-   // Input: string(value), Name of value to set
-   // Output: null()
-   //===================================================================================================
-   public function set_option($opt, $value = null) { $this->self["option.$opt"] = $value; }
-
-   //===================================================================================================
-   // Description: Gets option from class
-   // Input: string(opt), Name of option to retrieve
+   // Description: Gets option from class if any were set when initialized
+   // Input: string(key), Name of option key to retrieve
+   // Input: mixed(value), Name of value to set [optional]
+   // Input: boolean(clear), Treat null value as valid to clear [optional]
    // Output: string(value), Value of option
    //===================================================================================================
-   public function get_option($opt) { return $this->self["option.$opt"]; }
+   public function option($key, $value = null, $clear = false)
+   {
+      if (!is_null($value) || $clear) { $this->options[$key] = $value; }
+
+      return (isset($this->options[$key]) ? $this->options[$key] : null);
+   }
+
+   public function var($name, $value = null, $clear = null)
+   {
+      if (!is_null($value) || $clear) { $this->vars[$name] = $value; }
+
+      return (isset($this->vars[$name]) ? $this->vars[$name] : null);
+   }
 
    //===================================================================================================
    // Description: Debug relay function; available if class was constructed with Debug object.
-   // Input: int(level), Level of debug assertion (0-7)
-   // Input: string(mesg), Message to write to debug
+   // Input: int(level), Level of debug assertion (0-9)
+   // Input: string(message), Message to write to debug
    // Output: null()
    //===================================================================================================
-   public function debug($level,$mesg)
+   public function debug($level, $message)
    {
-      if (!isset($this->self['debug'])) { return; }
+      if (!isset($this->debug)) { return; }
 
-      $tracelevel = ($this->self["option.tracelevel"]) ? $this->self["option.tracelevel"] : 2;
+      $tracelevel = isset($this->options["tracelevel"]) ? $this->options["tracelevel"] : 2;
 
-      $this->self['debug']->trace($level,$mesg,$tracelevel);
+      $this->debug->trace($level,$message,$tracelevel);
    }
-
-   public function set($var, $val = null) { $this->data[$var] = $val; }
-   public function get($var) { return $this->data[$var]; }
 
    //===================================================================================================
    // Description: Checks whether an array is associative
@@ -99,12 +98,14 @@ class Base
    // Output: boolean(value), True if array was associative
    //===================================================================================================
    public function is_assoc($array) {
-      return (is_array($array) && 0 !== count(array_diff_key($array, array_keys(array_keys($array)))));
+       return (is_array($array) && 0 !== count(array_diff_key($array, array_keys(array_keys($array)))));
    }
 
    public function is_json($string) {
       return (!empty($string) && is_string($string) && is_array(json_decode($string,true)) && json_last_error() == 0);
    }
+
 }
 
 ?>
+
