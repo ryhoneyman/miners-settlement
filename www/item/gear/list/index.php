4227f1811cffe49f093e94a6a0ed9a77005c8e2e
ryhoneyman <ryhoneyman@gmail.com>
2023-04-16 22:13:31 +0000

    initial commit

---
+++ www/item/gear/list/index.php
@@ -0,0 +1,109 @@
+<?php
+include_once 'miners-settlement-init.php';
+include_once 'local/minersmain.class.php';
+
+$main = new MinersMain(array(
+   'debugLevel'     => 9,
+   'errorReporting' => true,
+   'sessionStart'   => true,
+   'memoryLimit'    => null,
+   'sendHeaders'    => true,
+   'database'       => true,
+   'input'          => true,
+   'html'           => true,
+));
+
+$main->title('Gear List');
+
+$input = $main->obj('input');
+$html  = $main->obj('html');
+
+$main->buildClass('constants','Constants',null,'local/constants.class.php');
+$main->buildClass('format','Format',null,'local/format.class.php');
+
+include 'ui/header.php';
+
+print dataDisplay($main);
+
+include 'ui/footer.php';
+
+?>
+<?php
+
+function dataDisplay($main)
+{
+   $return = '';
+
+   $main->var('gearList',getGear($main));
+
+   foreach ($main->var('gearList') as $gearType => $gearTypeList) {
+      $return .= gearDisplay($main,$gearType,$gearTypeList);
+   }
+
+   return $return;
+}
+
+function gearDisplay($main, $gearType, $gearTypeList)
+{
+   $gearType = ucwords(preg_replace('/\./',' ',$gearType));
+
+   $return = '<div class="row">'.
+             '<div class="col-12 col-xl-9 col-lg-10 col-md-12 col-sm-12">'.
+             '<div class="card card-outline card-success">'.
+             '<div class="card-header"><b class="text-xl">'.$gearType.'</b></div>'.
+             '<div class="card-body">'.
+             '<table class="table table-striped table-hover" style="width:auto;" border=0 cellpadding=10>'.
+             '<thead><tr class="text-yellow"><th></th><th>Name</th><th>Primary Stats</th><th>Elemental Stats</th></tr></thead><tbody>';
+
+   foreach ($gearTypeList as $entryId => $entryInfo) {
+      $gearName    = $entryInfo['label'];
+      $gearImage   = ($entryInfo['image']) ? sprintf("<img src='%s' height=50>",$entryInfo['image']) : '';;
+      $gearAttribs = json_decode($entryInfo['attributes'],true);
+      $gearPrimary = '';
+      $gearElement = '';
+
+      foreach ($main->obj('constants')->attribDisplay() as $attribName => $attribInfo) {
+         $rangeFormat = ($attribName == 'speed') ? '%1.2f | %1.2f' : '%d - %d';
+         $gearPrimary .= sprintf("<span class='badge' style='width:90px; background:%s; color:white;'>$rangeFormat <i class='fas fa-%s float-right'></i></span> ",
+                                 $attribInfo['color'],$gearAttribs["$attribName.min"],$gearAttribs["$attribName.max"],$attribInfo['icon']);
+      }
+
+      foreach ($main->obj('constants')->elementDisplay() as $elementName => $elementInfo) {
+         foreach (array('damage','resist') as $feature) {
+            if (!$gearAttribs["$elementName.$feature.min"]) { continue; }
+
+            $gearElement .= sprintf("<span style='color:%s;'>%s %s: %d - %d <i class='fa fa-%s'></i></span><br>",
+                                    $elementInfo['color'],strtoupper($elementName),strtoupper($feature),$gearAttribs["$elementName.$feature.min"],
+                                    $gearAttribs["$elementName.$feature.max"],$elementInfo['icon']);
+         }
+      }
+
+      $return .= sprintf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",$gearImage,$gearName,$gearPrimary,$gearElement);
+   }
+
+   $return .= '</tbody></table>'.
+              '</div>'.
+              '</div>'.
+              '</div>'.
+              '</div>';
+
+   return $return;
+}
+
+function getGear($main)
+{
+   $gearTypes = $main->obj('constants')->gearTypes();
+   $typeList  = implode(',',array_map(function($value) { return "'".preg_replace('/[^\w\.]/','',$value)."'"; },
+                                      array_unique(array_filter($gearTypes))));
+
+   $result   = $main->db()->query("select * from item where type in ($typeList) and active = 1",array('keyid' => 'id'));
+   $gearList = array();
+
+   foreach ($result as $resultId => $resultInfo) {
+      $gearList[$resultInfo['type']][$resultId] = $resultInfo;
+   }
+
+   return $gearList; 
+}
+
+?>
