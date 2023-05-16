b030922a121e30a8659be001fa37fcc3d0986901
ryhoneyman <ryhoneyman@gmail.com>
2023-05-16 20:25:01 +0000

    added activation code links and features display

--- www/profile/index.php
+++ www/profile/index.php
@@ -17,6 +17,8 @@ $main->title('Profile Management');
 
 include 'ui/header.php';
 
+print "<div class='mb-4'>User ID: ".$main->userId."</div>";
+
 ?>
 <div class="row">
 
@@ -65,13 +67,92 @@ include 'ui/header.php';
       </div>
    </div>
 
+   <div class="col-10 col-sm-6 col-md-6 col-lg-6 col-xl-3">
+      <div class="info-box">
+         <span class="info-box-icon bg-yellow elevation-1"><a href="/profile/activate/"><i class="fas fa-key" aria-hidden="true"></i></a></span>
+         <div class="ribbon-wrapper">
+            <div class="ribbon bg-danger">NEW</div>
+         </div>
+         <div class="info-box-content">
+            <span class="info-box-text"> <a href="/profile/activate/"><b>Activation Codes</b></a></span>
+            <span class="info-box-number" style="font-weight:normal;">
+               Unlock features with activation codes
+            </span>
+         </div>
+      </div>
+   </div>
 
 </div>
 
 <?php
 
-print "User ID: ".$main->userId;
+
+displayEntitlements($main);
 
 include 'ui/footer.php';
+
+?>
+<?php
+
+function displayEntitlements($main)
+{
+   $db     = $main->db();
+   $userId = $main->userId;
+
+   $entitleResult = $db->query("select data from profile_data where profile_id = '$userId' and name = 'entitlement'",array('multi' => 0));
+
+   if (!$entitleResult) { return false; } 
+
+   $entitlements = json_decode($entitleResult['data'],true);
+   $entitleList  = $db->query("select name,label,description,data from entitlement where active = 1");
+
+   if (!$entitleList) { return false; }
+
+   $featureContent = "<table border=0 class='table' style='width:auto;'>".
+                     "<tbody>";
+
+   foreach ($entitleList as $featureName => $featureInfo) {
+      if (!array_key_exists($featureName,$entitlements)) { continue; }
+      
+      $featureConstraints = $entitlements[$featureName];
+      $featureData        = json_decode($entitleList[$featureName]['data'],true);
+
+      $featureOptions = array();
+      foreach ($featureConstraints as $fcName => $fcValue) { $featureOptions[] = sprintf("%s %s",$fcValue,$fcName); }
+
+      $featureContent .= sprintf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
+                                 sprintf("<i class='text-lg fa fa-%s %s'>",$featureData['icon'],$featureData['class']),$featureInfo['label'],$featureInfo['description'],implode("<br>",$featureOptions));
+   }
+
+   $featureContent .= "</tbody>".
+                      "</table>";
+
+   print "<div class='mt-4'></div>";
+   print displayRow(displayCard(array('container' => 'col-12 col-xl-6 col-lg-9 col-md-12 col-sm-12', 'header' => 'Activated Features'),$featureContent)); 
+}
+
+function displayRow($content)
+{
+   return "<div class='row'>".
+          $content.
+          "</div>";
+}
+
+function displayCard($cardProperties, $content)
+{
+   $containerClass = $cardProperties['container'] ?: 'col-12 col-xl-3 col-lg-6 col-md-6 col-sm-12';
+   $cardClass      = $cardProperties['card'] ?: 'card-primary';
+   $cardHeader     = $cardProperties['header'] ?: 'Card';
+
+   return "<div class='$containerClass'>".
+          "    <div class='card $cardClass'>".
+          "       <div class='card-header'><b>$cardHeader</b></div>".
+          "       <div class='card-body'>".
+          "       ".$content.
+          "       </div>".
+          "   </div>".
+          "</div>";
+
+}
 
 ?>
