482bae96d65dbe011dbc9b82cf0fdc6ae9f5bb45
ryhoneyman <ryhoneyman@gmail.com>
2023-05-27 16:55:18 +0000

    added select2_template function

--- www/assets/js/ms.js
+++ www/assets/js/ms.js
@@ -2,3 +2,28 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+function select2_template(obj)
+{
+   var data     = $(obj.element).data();
+   var text     = $(obj.element).text();
+   var type     = $(obj.element).prop('nodeName');
+   var image    = (data && data['image']) ? data['image'] : null;
+   var template = '';
+
+   if (type == 'OPTGROUP') {
+      var label = $(obj.element).attr('label');
+      template  = $('<div><span>' + label + '</span></div>');
+   }
+   else if (image) {
+      template = $('<div><img src=\"' + image + '\" style=\"width:25px; height:25px;\"/> <span style=\"font-weight:bold;\">' + text + '</span></div>');
+   }
+   else if (obj.id === '') {
+      template = $('<div><span>' + text + '</span></div>');
+   }
+   else {
+      template = $('<div><span style=\"font-weight:bold;\">' + text + '</span></div>');
+   }
+
+   return template;
+}
+
