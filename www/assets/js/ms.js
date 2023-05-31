ab264ca41fa788368e4102de20c909f1daf0d961
ryhoneyman <ryhoneyman@gmail.com>
2023-05-31 18:57:02 +0000

    add icon and css control to select2_template

--- www/assets/js/ms.js
+++ www/assets/js/ms.js
@@ -8,6 +8,8 @@ function select2_template(obj)
    var text     = $(obj.element).text();
    var type     = $(obj.element).prop('nodeName');
    var image    = (data && data['image']) ? data['image'] : null;
+   var icon     = (data && data['icon']) ? data['icon'] : null;
+   var css      = (data && data['css']) ? data['css'] : null;
    var template = '';
 
    if (type == 'OPTGROUP') {
@@ -15,13 +17,23 @@ function select2_template(obj)
       template  = $('<div><span>' + label + '</span></div>');
    }
    else if (image) {
-      template = $('<div><img src=\"' + image + '\" style=\"width:25px; height:25px;\"/> <span style=\"font-weight:bold;\">' + text + '</span></div>');
+      template = $('<div class=\"' + css + '\"><img src=\"' + image + '\" style=\"width:25px; height:25px;\"/> <span style=\"font-weight:bold;\">' + text + '</span></div>');
+   }
+   else if (icon) {
+      var icons = icon.split(";");
+      var iconList = '';
+
+      icons.forEach((item) => { 
+         iconList += '<i class=\"fa ' + item + '\"/></i> ';
+      });
+
+      template = $('<div class=\"' + css + '\">' + iconList + '<span style=\"font-weight:bold;\">' + text + '</span></div>');
    }
    else if (obj.id === '') {
-      template = $('<div><span>' + text + '</span></div>');
+      template = $('<div class=\"' + css + '\"><span>' + text + '</span></div>');
    }
    else {
-      template = $('<div><span style=\"font-weight:bold;\">' + text + '</span></div>');
+      template = $('<div class=\"' + css + '\"><span style=\"font-weight:bold;\">' + text + '</span></div>');
    }
 
    return template;
