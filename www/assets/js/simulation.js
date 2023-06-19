cd4e481512bf3cac9fa1860ca9b3dc77a56bede5
ryhoneyman <ryhoneyman@gmail.com>
2023-06-19 01:43:37 +0000

    updated rune function calls

--- www/assets/js/simulation.js
+++ www/assets/js/simulation.js
@@ -13,12 +13,6 @@ var statsOptions = {
 
 $('.stats').select2(statsOptions);
 
-var runesOptions = {
-   maximumSelectionLength: 5
-}
-
-$('.runes').select2(runesOptions);
-
 $('#monster').select2();
 $('#iterations').select2({minimumResultsForSearch: -1});
 
@@ -26,25 +20,11 @@ function updateScalableGear(gearType, clearStats = false, refreshRunes = false)
 {
    $('#'+gearType+'-stats').html();
 
-   var gearName = $('#'+gearType).val();
+   var gearHash = $('#'+gearType).val();
 
-   $.ajax({url:'/simulation/scalable/gearstats.php?clear='+clearStats+'&type='+gearType+'&name='+gearName, async:true}).done(function(data) { $('#'+gearType+'-stats').html(data); $('.stats').select2(statsOptions); });
+   $.ajax({url:'/simulation/scalable/gearstats.php?clear='+clearStats+'&type='+gearType+'&hash='+gearHash, async:true}).done(function(data) { $('#'+gearType+'-stats').html(data); $('.stats').select2(statsOptions); });
 
-   if (refreshRunes) { updateScalableRunes(); }
-}
-
-function updateScalableRunes()
-{
-   $('#rune-section').html();
-
-   var equipState = {}
-   const gearTypes = ['weapon','shield','ring','amulet','weapon-skin','shield-skin'];
-
-   gearTypes.forEach((gearType) => {
-      equipState[gearType] = $('#'+gearType).val();
-   });
-
-   $.ajax({url:'/simulation/scalable/runes.php?state='+JSON.stringify(equipState), async:true}).done(function(data) { $('#rune-section').html(data); $('.runes').select2(runesOptions); });
+   if (refreshRunes) { updateRunes('scalable'); }
 }
 
 function loadScalableResults() 
