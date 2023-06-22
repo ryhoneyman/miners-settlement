ee7940f37303baabeef1bfa6f29df1970aad5aec
ryhoneyman <ryhoneyman@gmail.com>
2023-06-22 13:33:41 +0000

    updated all list references, session lookups

--- www/profile/player/build/index.php
+++ www/profile/player/build/index.php
@@ -21,61 +21,79 @@ $main->title('Build Management');
 $input = $main->obj('input');
 $alte  = $main->obj('adminlte');
 
-$main->fetchPlayerList();
-$main->fetchPlayerBuildList();
-$main->fetchPlayerGearList();
+$main->var('sessionName','playerbuild/pageInput');
 
-$player    = $input->get('player','alphanumeric,dot,dash,underscore,space');
-$build     = $input->get('build','alphanumeric,dot,dash,underscore,space');
-$buildName = $input->get('build-name','alphanumeric,dot,dash,underscore,space,forwardslash');
-$load      = ($input->isDefined('load')) ? true : false;
-$save      = ($input->isDefined('save')) ? true : false;
+$playerHash = $input->get('player','alphanumeric,dot,dash,underscore,space');
+$buildHash  = $input->get('build','alphanumeric,dot,dash,underscore,space');
+$buildName  = $input->get('build-name','alphanumeric,dot,dash,underscore,space,forwardslash');
+$load       = ($input->isDefined('load')) ? true : false;
+$delete     = ($input->isDefined('delete')) ? true : false;
+$save       = ($input->isDefined('save')) ? true : false;
+$clear      = ($input->isDefined('clear')) ? true : false;
 
-
-$sessionInput = $main->sessionValue('playerbuild/pageInput') ?: array();
+$sessionInput = $main->sessionValue($main->var('sessionName')) ?: array();
 $pageInput    = processInput($sessionInput);
 
-if ($load && $build) {
-   $playerBuildList = $main->var('playerBuildList');
+if (($load || $delete) && $buildHash) {
+   $playerBuildList     = $main->getPlayerBuildList(); 
+   $playerBuildHashList = $main->getPlayerBuildHashList();
+   $buildId             = $playerBuildHashList[$buildHash] ?: null;
   
-   if (array_key_exists($build,$playerBuildList)) { 
-      $buildData = $playerBuildList[$build];
-      $pageInput = json_decode($buildData['build'],true);
+   if ($buildId) { 
+      if ($load) {
+         $buildData = $playerBuildList[$buildId];
+         $pageInput = json_decode($buildData['build'],true);
 
-      $buildName = $buildData['name'];
-      $player    = $buildData['player_id'];
+         $buildName  = $buildData['name'];
+         $playerHash = $main->hashPlayerId($buildData['player_id']);
 
-      $pageInput['build-name'] = $buildName;
-      $pageInput['player']     = $player;
+         $pageInput['build-name'] = $buildName;
+         $pageInput['player']     = $playerHash;
+      }
+      else if ($delete) {
+         $rc = $main->db()->bindExecute('delete from player_build where id = ?','s',array($buildId));
+         if (!$rc) { $this->error($main->db()->error()); }
+      }
    }
 }
 
-$main->sessionValue('playerbuild/pageInput',array_diff_key($pageInput,array_flip(array('save','load'))));
+$main->sessionValue($main->var('sessionName'),array_diff_key($pageInput,array_flip(array('save','load'))));
 
 if ($save) {
-   if (!$player || !$buildName) { $main->error("Player and Build Name are required."); }
+   if (!$playerHash || !$buildName) { $main->error("Player and Build Name are required."); }
    else {
       $buildData = array();
 
       foreach ($main->obj('constants')->gearTypes() as $gearType => $gearTypeLabel) {
          $buildData[$gearType] = $pageInput[$gearType];
       }
+
       $buildData['runes'] = $pageInput['runes'];
 
+      $playerHashList = $main->getPlayerHashList();
+      $playerId       = $playerHashList[$playerHash];
+      
       $rc = $main->db()->bindExecute("insert into player_build (name,player_id,build,created,updated) values (?,?,?,now(),now()) ".
                                      "on duplicate key update build = values(build), updated = values(updated)",
-                                     "sis",array(array($buildName,$player,json_encode($buildData,JSON_UNESCAPED_SLASHES))));
+                                     "sis",array(array($buildName,$playerId,json_encode($buildData,JSON_UNESCAPED_SLASHES))));
 
       if (!$rc) { $this->error($main->db()->error()); }
       else { 
-         $build = $main->db()->insertId();
-         $main->fetchPlayerBuildList(); 
+         $buildId   = $main->db()->insertId();
+         $buildHash = $main->hashPlayerBuildId($buildId);
       }
    }
 }
+else if ($clear) {
+   $main->sessionValue($main->var('sessionName'),null,true);
+   $playerHash = null;
+   $buildHash  = null;
+   $buildName  = null;
+   $pageInput  = array();
+}
 
-$main->var('player',$player);
-$main->var('build',$build);
+$main->var('player',$playerHash);
+$main->var('build',$buildHash);
 $main->var('buildName',$buildName);
 $main->var('pageInput',$pageInput);
 
@@ -94,17 +112,19 @@ include 'ui/footer.php';
 
 function pageDisplay($main)
 {
-   if (!$main->var('playerList')) { $return = "No players found, add them in <a href='/profile/player'>Player Management</a>"; }
+   $html = $main->obj('html');
+
+   if (!$main->getPlayerList()) { $return = "No players found, add them in <a href='/profile/player'>Player Management</a>"; }
    else {
-      $return = $main->obj('html')->startForm(array('method' => 'post')).
+      $return = $html->startForm(array('method' => 'post')).
                 playerBuildSelect($main).
-                $main->obj('html')->endForm().
-                $main->obj('html')->startForm(array('method' => 'post')).
+                $html->endForm().
+                $html->startForm(array('method' => 'post')).
                 gearDisplay($main).
                 runeDisplay($main).
                 playerSelect($main).
-                $main->obj('html')->endForm().
-                "<script src='/assets/js/equipmentmanager.js?t=".$main->now."' type='text/javascript'></script>\n".
+                $html->endForm().
+                "<script src='/assets/js/buildmanager.js?t=".$main->now."' type='text/javascript'></script>\n".
                 "<script src='/assets/js/runeselect.js?t=".$main->now."' type='text/javascript'></script>\n".
                 "<link rel='stylesheet' href='/assets/css/simulation.css?t=".$main->now."'>\n";
 
@@ -115,13 +135,6 @@ function pageDisplay($main)
    }
 
    return $return;
-}
-
-function playerDisplay($main)
-{
-   $db         = $main->db();
-   $html       = $main->obj('html');
-   $userId     = $main->userId;
 }
 
 function runeDisplay($main)
@@ -144,13 +157,7 @@ function gearDisplay($main)
    $constants = $main->obj('constants');
    $pageInput = $main->var('pageInput');
 
-   $main->fetchPlayerGearList();
-
-   $gearList = array();
-
-   foreach ($main->var('playerGearList') as $gearId => $gearInfo) {
-      $gearList[$gearInfo['type']][$gearId] = $gearInfo;
-   }
+   $gearList = $main->getPlayerGearListByType();
 
    foreach ($constants->gearTypes() as $gearType => $gearTypeLabel) {
       $typeList   = array('' => "Select $gearTypeLabel", 'none' => 'NONE');
@@ -161,12 +168,15 @@ function gearDisplay($main)
       foreach ($gearTypeList as $gearId => $gearInfo) {
          $gearHash  = $main->hashPlayerGearId($gearId);
          $gearStats = json_decode($gearInfo['stats'],true);
+         $gearImage = $gearInfo['image'];
+         $gearLabel = str_replace("'",'&apos;',$gearInfo['label']);
+         $gearLevel = ($gearStats['level'] > 0) ? sprintf(" +%d ",$gearStats['level']) : '';
 
          $typeList[$gearHash] = $gearInfo['label'];
 
          $gearItemStats = formatGearStats($main,$gearStats);
 
-         $selectOpts['data'][$gearHash]['raw'] = sprintf("<img src=&quot;%s&quot; width=25px height=25px> %s : [ %s ]",$gearInfo['image'],str_replace("'",'&apos;',$gearInfo['label']),$gearItemStats);
+         $selectOpts['data'][$gearHash]['raw'] = sprintf("<img src=&quot;%s&quot; width=25px height=25px> %s%s : [ %s ]",$gearImage,$gearLevel,$gearLabel,$gearItemStats);
       }
 
       $typeSelected = $pageInput[$gearType];
@@ -206,13 +216,13 @@ function playerSelect($main)
    $alte           = $main->obj('adminlte');
    $selectedPlayer = $main->var('player');
    $buildName      = $main->var('buildName');
-   $playerList     = $main->var('playerList') ?: array();
+   $playerList     = $main->getPlayerList() ?: array();
 
    $selectPlayers = array();
-   foreach ($playerList as $playerName => $playerInfo) { $selectPlayers[$playerInfo['id']] = $playerName; }
+   foreach ($playerList as $playerId => $playerInfo) { $selectPlayers[$main->hashPlayerId($playerId)] = $playerInfo['name']; }
 
    $selectContent = "<div class='input-group'>".
-                    $html->select('player',$selectPlayers).
+                    $html->select('player',$selectPlayers,$selectedPlayer).
                     $html->inputText('build-name',$buildName,null,null,array('required' => true, 'placeholder' => 'Build Name')).
                     "<span class='input-group-append'>".
                     $html->submit('save','Save',array('class' => 'btn btn-primary btn-sm')).
@@ -226,15 +236,20 @@ function playerBuildSelect($main)
    $html            = $main->obj('html');
    $alte            = $main->obj('adminlte');
    $selectedBuild   = $main->var('build');
-   $playerBuildList = $main->var('playerBuildList') ?: array();
+   $playerBuildList = $main->getPlayerBuildList(array('skipCache' => true)) ?: array();
 
    $buildList = array();
-   foreach ($playerBuildList as $playerBuildId => $playerBuildInfo) { $buildList[$playerBuildId] = sprintf("%s (%s)",$playerBuildInfo['player_name'],$playerBuildInfo['name']); }
+   foreach ($playerBuildList as $playerBuildId => $playerBuildInfo) { 
+      $playerBuildHash = $main->hashPlayerBuildId($playerBuildId);
+      $buildList[$playerBuildHash] = sprintf("%s (%s)",$playerBuildInfo['player_name'],$playerBuildInfo['name']); 
+   }
 
    $selectContent = "<div class='input-group'>".
                     $html->select('build',$buildList,$selectedBuild).
                     "<span class='input-group-append'>".
                     $html->submit('load','Load',array('class' => 'btn btn-warning btn-sm')).
+                    $html->submit('delete','Delete',array('class' => 'btn btn-danger btn-sm')).
+                    $html->submit('clear','Clear',array('class' => 'btn btn-success btn-sm')).
                     "</span></div>";
 
    return $alte->displayRow($alte->displayCard($selectContent,array('title' => 'Player Builds', 'container' => 'col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-3', 'card' => 'card-secondary')));
@@ -245,7 +260,7 @@ function processInput($sessionInput)
    $postInput = $_POST;
 
    // Empty runes are not passed to us by the browser, so we initialize them to empty
-   if (!array_key_exists('runes',$postInput) && $postInput['start']) { $postInput['runes'] = array(); }
+   if (!array_key_exists('runes',$postInput) && ($postInput['save'] || $postInput['clear'])) { $postInput['runes'] = array(); }
 
    $return = array_merge($sessionInput,$postInput);
 
