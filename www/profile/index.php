0b68e43ffdf1c3b2568be892f5eec01c937c777f
ryhoneyman <ryhoneyman@gmail.com>
2023-06-22 13:33:10 +0000

    updated to infobox components

--- www/profile/index.php
+++ www/profile/index.php
@@ -11,83 +11,30 @@ $main = new MinersMain(array(
    'database'       => true,
    'input'          => true,
    'html'           => true,
+   'adminlte'       => true,
 ));
 
 $main->title('Profile Management');
+
+$alte = $main->obj('adminlte');
 
 include 'ui/header.php';
 
 print "<div class='mb-4'>User ID: ".$main->userId."</div>";
 
-?>
-<div class="row">
-
-   <div class="col-10 col-sm-6 col-md-6 col-lg-6 col-xl-3">
-      <div class="info-box">
-         <span class="info-box-icon bg-gray elevation-1"><a href="#"><i class="fas fa-link" aria-hidden="true"></i></a></span>
-         <div class="ribbon-wrapper">
-            <div class="ribbon bg-warning text-bold" style="font-size:0.5em;">COMING SOON</div>
-         </div>
-         <div class="info-box-content">
-            <span class="info-box-text"> <a href="#"><b>Link Profile</b></a></span>
-            <span class="info-box-number" style="font-weight:normal;">
-               Link profile to email
-            </span>
-         </div>
-      </div>
-   </div>
-
-   <div class="col-10 col-sm-6 col-md-6 col-lg-6 col-xl-3">
-      <div class="info-box">
-         <span class="info-box-icon bg-success elevation-1"><a href="/profile/player/"><i class="fas fa-users" aria-hidden="true"></i></a></span>
-         <div class="ribbon-wrapper">
-            <div class="ribbon bg-danger">NEW</div>
-         </div>
-         <div class="info-box-content">
-            <span class="info-box-text"> <a href="/profile/player/"><b>Player Management</b></a></span>
-            <span class="info-box-number" style="font-weight:normal;">
-               Manage players on profile
-            </span>
-         </div>
-      </div>
-   </div>
-
-   <div class="col-10 col-sm-6 col-md-6 col-lg-6 col-xl-3">
-      <div class="info-box">
-         <span class="info-box-icon bg-success elevation-1"><a href="/profile/gear/"><i class="fas fa-swords" aria-hidden="true"></i></a></span>
-         <div class="ribbon-wrapper">
-            <div class="ribbon bg-danger">NEW</div>
-         </div>
-         <div class="info-box-content">
-            <span class="info-box-text"> <a href="/profile/gear/"><b>Gear Management</b></a></span>
-            <span class="info-box-number" style="font-weight:normal;">
-               Manage gear on profile
-            </span>
-         </div>
-      </div>
-   </div>
-
-   <div class="col-10 col-sm-6 col-md-6 col-lg-6 col-xl-3">
-      <div class="info-box">
-         <span class="info-box-icon bg-yellow elevation-1"><a href="/profile/activate/"><i class="fas fa-key" aria-hidden="true"></i></a></span>
-         <div class="ribbon-wrapper">
-            <div class="ribbon bg-danger">NEW</div>
-         </div>
-         <div class="info-box-content">
-            <span class="info-box-text"> <a href="/profile/activate/"><b>Activation Codes</b></a></span>
-            <span class="info-box-number" style="font-weight:normal;">
-               Unlock features with activation codes
-            </span>
-         </div>
-      </div>
-   </div>
-
-</div>
-
-<?php
-
-
-displayEntitlements($main);
+print $alte->displayRow(
+         $alte->infoBox('Link Profile','#','Link profile to email',
+                        array('icon' => 'fa-link', 'icon-bg' => 'bg-gray', 'ribbon' => 'coming soon', 'ribbon-bg' => 'bg-warning text-bold text-tiny')).
+         $alte->infoBox('Player Management','/profile/player/','Manage players on profile',
+                        array('icon' => 'fa-users', 'icon-bg' => 'bg-success')).
+         $alte->infoBox('Gear Management','/profile/gear/','Manage gear on profile',
+                        array('icon' => 'fa-shield-alt', 'icon-bg' => 'bg-success')).
+         $alte->infoBox('Build Management','/profile/player/build/','Manage builds on profile',
+                        array('icon' => 'fa-user-shield', 'icon-bg' => 'bg-success', 'ribbon' => 'new', 'ribbon-bg' => 'bg-danger text-bold')).
+         $alte->infoBox('Activation Codes','/profile/activate/','Unlock features with activation codes',
+                        array('icon' => 'fa-key', 'icon-bg' => 'bg-warning'))
+      ).
+      displayEntitlements($main);
 
 include 'ui/footer.php';
 
@@ -97,6 +44,7 @@ include 'ui/footer.php';
 function displayEntitlements($main)
 {
    $db     = $main->db();
+   $alte   = $main->obj('adminlte');
    $userId = $main->userId;
 
    $entitleResult = $db->query("select data from profile_data where profile_id = '$userId' and name = 'entitlement'",array('multi' => 0));
@@ -127,32 +75,8 @@ function displayEntitlements($main)
    $featureContent .= "</tbody>".
                       "</table>";
 
-   print "<div class='mt-4'></div>";
-   print displayRow(displayCard(array('container' => 'col-12 col-xl-6 col-lg-9 col-md-12 col-sm-12', 'header' => 'Activated Features'),$featureContent)); 
-}
-
-function displayRow($content)
-{
-   return "<div class='row'>".
-          $content.
-          "</div>";
-}
-
-function displayCard($cardProperties, $content)
-{
-   $containerClass = $cardProperties['container'] ?: 'col-12 col-xl-3 col-lg-6 col-md-6 col-sm-12';
-   $cardClass      = $cardProperties['card'] ?: 'card-primary';
-   $cardHeader     = $cardProperties['header'] ?: 'Card';
-
-   return "<div class='$containerClass'>".
-          "    <div class='card $cardClass'>".
-          "       <div class='card-header'><b>$cardHeader</b></div>".
-          "       <div class='card-body'>".
-          "       ".$content.
-          "       </div>".
-          "   </div>".
-          "</div>";
-
+   return "<div class='mt-4'></div>".
+          $alte->displayRow($alte->displayCard($featureContent,array('container' => 'col-12 col-xl-6 col-lg-9 col-md-12 col-sm-12', 'header' => 'Activated Features'))); 
 }
 
 ?>
