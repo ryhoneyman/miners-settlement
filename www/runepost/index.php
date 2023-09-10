1e5aa768c5ba26b9b8914bc1fe5f060a7ad6beb9
ryhoneyman <ryhoneyman@gmail.com>
2023-09-10 20:05:17 +0000

    added filter selector pulldown and item click filter

--- www/runepost/index.php
+++ www/runepost/index.php
@@ -21,22 +21,73 @@ $main->title('Runeposts');
 $input = $main->obj('input');
 $html  = $main->obj('html');
 
+$selectedItem = $input->get('item','alphanumeric,dash',null);
+$clear        = $input->get('clear','alphanumeric');
+
+if ($clear) { $selectedItem = null; }
+
+$main->var('selectedItem',$selectedItem);
+
 include 'ui/header.php';
+
+print "
+<style>
+.select2-results__option { line-height:1.0; }
+.select2-container--default .select2-results>.select2-results__options { max-height: 350px; }
+</style>
+";
 
 print dataDisplay($main);
 
+print "
+<script type='text/javascript'>
+   $('.gear').select2({
+      templateSelection: select2_template,
+      templateResult: select2_template,
+   });
+</script>
+";
+
+
 include 'ui/footer.php';
+
+print "<script>window.history.replaceState(null, null, window.location.pathname);</script>\n";
 
 ?>
 <?php
 
 function dataDisplay($main)
 {
-   $return = '';
+   $html = $main->obj('html');
+
+   $return = $html->startForm();
 
    $main->var('runewordList',getRunewords($main));
    $main->var('runeList',getRunes($main));
    $main->var('gearInfo',getSupportingGearInfo($main));
+
+   // Build the pulldown list of items
+   $selectItem = array('' => 'Select an Item');
+   $selectOpts = array('class' => 'form-control gear', 'script' => 'onchange="autoChange(this.value)"');
+   $gearTypes  = $main->obj('constants')->gearTypes();
+
+   $gearInfo      = $main->var('gearInfo');
+   $selectedItem  = $main->var('selectedItem');
+   $selectedMatch = (is_null($selectedItem)) ? true : false;
+
+   foreach ($gearInfo as $itemName => $itemData) {
+      if ($itemName == $selectedItem) { $selectedMatch = true; }
+
+      $selectItem[$gearTypes[$itemData['type']]][$itemName] = $itemData['label'];
+      $selectOpts['data'][$itemName]['image'] = $itemData['image'];
+   }
+   array_multisort($selectItem);
+
+   // If we received an unknown selected item, just null it
+   if (!$selectedMatch) { 
+      $selectedItem = null; 
+      $main->var('selectedItem',$selectedItem);
+   }
 
    $areaNav = array();
    foreach (array_keys($main->var('runewordList')) as $areaName) {
@@ -49,9 +100,18 @@ function dataDisplay($main)
    
    $return .= '<div class="mb-4"></div>'; 
 
+   $return .= "<div class='input-group mb-4' style='width:fit-content;'>".
+              $html->select('item',$selectItem,$selectedItem,$selectOpts).
+              "<span class='input-group-append'>".
+              $html->submit('select','Select',array('class' => 'btn btn-primary btn-sm')).
+              $html->submit('clear','Clear',array('class' => 'btn btn-success btn-sm')).
+              "</span></div>";
+
    foreach ($main->var('runewordList') as $areaName => $areaPosts) {
       $return .= areaDisplay($main,$areaName,$areaPosts);
    }
+
+   $return .= $html->endForm();
 
    return $return;
 }
@@ -66,9 +126,18 @@ function areaDisplay($main, $areaName, $areaPosts)
              '<div class="card-body">'.
              '<div class="row">';
 
+   $areaMatched = false;
+
    foreach ($areaPosts as $postName => $postInfo) {
-      $return .= runepostDisplay($main,$postName,$postInfo);
+      $rpDisplay = runepostDisplay($main,$postName,$postInfo);
+
+      if ($rpDisplay) { 
+         $return .= $rpDisplay; 
+         $areaMatched = true;
+      }
    }
+
+   if ($areaMatched === false) { return ''; }
 
    $return .= '</div>'.
               '</div>'.
@@ -90,8 +159,11 @@ function runepostDisplay($main, $postName, $postInfo)
              '<table class="table table-striped table-hover" border=0 cellpadding=10>'.
              '<thead><tr class="text-yellow"><th style="width:25%">Name</th><th style="width:25%">Gear</th><th style="width:35%">Effects</th><th style="width:15%">Required Runes</th></tr></thead><tbody>';
 
-   $runeList = $main->var('runeList');
-   $gearInfo = $main->var('gearInfo');
+   $runeList     = $main->var('runeList');
+   $gearInfo     = $main->var('gearInfo');
+   $selectedItem = $main->var('selectedItem');
+
+   $itemCount = 0;
 
    foreach ($postInfo as $entryId => $entryInfo) {
       $runewordName  = $entryInfo['label'];
@@ -99,12 +171,18 @@ function runepostDisplay($main, $postName, $postInfo)
       $entryAttrib   = json_decode($entryInfo['attributes'],true);
       $itemRequired  = json_decode($entryInfo['requires'],true) ?: array('');
       $itemList      = array();
+      $filterMatched = (is_null($selectedItem)) ? true : false;
 
       foreach ($itemRequired as $itemName) {
+         if (!is_null($selectedItem) && $itemName == $selectedItem) { $filterMatched = true; }
+
          $itemLabel  = $gearInfo[$itemName]['label'] ?: 'None';
          $itemImage  = $gearInfo[$itemName]['image'];
-         $itemList[] = ($itemImage) ? sprintf("<div class='mb-1'><img src='%s' height=50 data-toggle='tooltip' title=\"%s\"> <span class='text-green'>$itemLabel</span></div>",$itemImage,$itemLabel) : $itemLabel;
+         $itemList[] = ($itemImage) ? sprintf("<div class='mb-1'><a href='?item=$itemName'><img src='%s' height=50 data-toggle='tooltip' title=\"%s\"> ".
+                                              "<span class='text-green'>$itemLabel</span></a></div>",$itemImage,$itemLabel) : $itemLabel;
       }
+
+      if ($filterMatched === false) { continue; }
 
       $itemInsert    = implode('',$itemList);
       $requiredRunes = array();
@@ -124,6 +202,8 @@ function runepostDisplay($main, $postName, $postInfo)
          $runeEffects = '<code style="color:#aaaaaa;">'.$main->obj('format')->effects($entryAttrib,true).'</code>'; 
       }
 
+      $itemCount++;
+
       $return .= sprintf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",$runewordName,$itemInsert,$runeEffects,implode('',array_reverse($requiredRunes)));
    }
 
@@ -131,6 +211,8 @@ function runepostDisplay($main, $postName, $postInfo)
               '</div>'.
               '</div>'.
               '</div>';
+
+   if (!$itemCount) { return ''; }
 
    return $return;
 }
@@ -157,7 +239,7 @@ function getSupportingGearInfo($main)
 
    $nameList = implode(',',array_map(function($value) { return "'".preg_replace('/[^\w\-]/','',$value)."'"; },
                               array_unique(array_filter(array_keys($gearNames)))));
-   $itemList = $main->db()->query("select * from item where name in ($nameList)",array('keyid' => 'name'));
+   $itemList = $main->db()->query("select * from item where name in ($nameList) order by tier asc, ranking asc, name asc",array('keyid' => 'name'));
 
    return $itemList;
 }
