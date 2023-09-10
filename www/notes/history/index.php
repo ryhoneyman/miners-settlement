cb1c0c841d1c67f37c2c0bc9c032f8379e3d41a0
ryhoneyman <ryhoneyman@gmail.com>
2023-09-10 20:21:50 +0000

    uprev version

--- www/notes/history/index.php
+++ www/notes/history/index.php
@@ -11,7 +11,56 @@ $main->title('Patch Notes History');
 include 'ui/header.php';
 
 $versionList = array(
-   '1.5.2' => array(
+   array(
+      'version' => '1.6.3',
+      'datetime' => '2023-09-10 20:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added item filter to Runeposts',
+            'Added item links back to Item Aanlytics from Gear List',
+            'Enabled auto-submit for pulldowns in Monster Viewer, Item Analytics, and Runeposts',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.6.2',
+      'datetime' => '2023-08-31 04:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added Mitar Forge Crafting page',
+         ),
+         'backend' => array(
+            'Restructured all crafting data structures',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.6.1',
+      'datetime' => '2023-07-15 04:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Fixed runeword selection on Scalable Simulation', 
+            'Runewords can now recognize multiple items to activate',
+         ),
+         'backend' => array(
+            'Updated all runeword requirement structures',
+            'Added runeposts with area-exclusive runewords',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.6.0',
+      'datetime' => '2023-06-22 13:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added Build Simulation',
+            'Added Build Management',
+            'Grouped items by type in Gear Management',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.5.2',
       'datetime' => '2023-06-18 01:30 UTC',
       'changes' => array(
          'backend' => array(
@@ -19,10 +68,333 @@ $versionList = array(
          ),
       ),
    ),
+   array(
+      'version' => '1.5.1',
+      'datetime' => '2023-06-15 05:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added Battle Log to Scalable Simulation',
+            'Unlocked Simulation for all users (default 10 iterations)',
+         ),
+         'backend' => array(
+            'Updated stun-resist property on all runewords',
+            'Updated all dragon attributes',
+            'Corrected extra defense to be activated on attacker hit (battle engine)',
+            'Corrected stun resist to mitigate a percentage of stun, not the entire thing (battle engine)',
+            'Corrected defense lower stacking from additive to multiplicative (battle engine)',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.5.0',
+      'datetime' => '2023-06-12 18:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added Monster Viewer',
+         ),
+         'backend' => array(
+            'Added most monsters in the game to date (still missing some Shadow Order Dungeon)',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.4.1',
+      'datetime' => '2023-06-05 18:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Updated color/icons for runewords with elemental in Runeposts',
+         ),
+         'backend' => array(
+            'Refactored stored game constants for access uniformity',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.4.0',
+      'datetime' => '2023-06-03 20:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added Simulation section and Scalable Simulation tool',
+            'Placed info box link to profile on homepage',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.3.3',
+      'datetime' => '2023-05-27 17:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added images to pulldown in Item Analytics',
+            'Adjusted table formatting/alignment in Runeposts',
+         ),
+         'backend' => array(
+            'Added new Einlor rings',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.3.2',
+      'datetime' => '2023-05-19 16:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Final corrections to rounding issues in Item Analytics',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.3.0',
+      'datetime' => '2023-05-16 20:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Corrected mininum/maxinum bounds due to rounding issues on Item Analytics',
+            'Added new links on Home page',
+            'Added Activation Codes link and feature display to Profile page',
+            'New Activation Codes page available in Profile',
+         ),
+         'backend' => array(
+            'Creation of database elements to support activation codes',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.2.4',
+      'datetime' => '2023-05-12 21:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added Item Enhancement page',
+         ),
+         'backend' => array(
+            'Adjustments to simulator CLI',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.2.3',
+      'datetime' => '2023-05-10 14:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Runeposts page now shows runes in order that matches the game',
+         ),
+         'backend' => array(
+            'Added Big Surprise and Pumpkin Reaper runewords',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.2.2',
+      'datetime' => '2023-05-10 14:30 UTC',
+      'changes' => array(
+         'backend' => array(
+            'Added Christmas amulets to the database',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.2.1',
+      'datetime' => '2023-05-09 14:30 UTC',
+      'changes' => array(
+         'backend' => array(
+            'Updated incorrect data on runewords for Glory Shield and Spear of the Gods',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.2.0',
+      'datetime' => '2023-05-06 23:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added Scheme Crafting area with basic schemes', 
+            'Profile area now has Player and Gear sections',
+            'Added ability to add/remove players and gear on profile',
+            'Added item sharing on Item Analytics',
+            'Added weapon and shield skins to database',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.1.6',
+      'datetime' => '2023-04-28 14:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Fixed form handler from closing early preventing calculation on Item Analytics',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.1.5',
+      'datetime' => '2023-04-27 14:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Adjusted decimal notation to support commas and periods',
+            'Fixed invalid input warning on valid items',
+            'Added level appropriate bounds checking for items',
+         ),
+         'backend' => array(
+            'Decreased debug level and error outputing on production',
+            'Deployed item database',
+            'Linked items in Gear List to database',
+            'Updated level from 10 to 15',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.1.4',
+      'datetime' => '2023-04-24 15:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Update attribution and license footer',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.1.3',
+      'datetime' => '2023-04-22 05:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Added jump links and return to top buttons on Runeposts page',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.1.2',
+      'datetime' => '2023-04-16 22:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Gear List area created',
+         ),
+         'backend' => array(
+            'Updated elemental effects',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.1.1',
+      'datetime' => '2023-04-15 05:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Runeposts area created',
+            'Cleanup page layout inconsistencies',
+            'Updated title display on all pages',
+         ),
+         'backend' => array(
+            'Added Weapon and Shield Skin to Simulator',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.1.0',
+      'datetime' => '2023-04-06 04:00 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Updated Admin LTE UI framework to v3.2.0',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.0.3',
+      'datetime' => '2023-04-04 05:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Updated title method for displaying page title',
+         ),
+         'backend' => array(
+            'Added DPS to output in Simulator',
+         ),
+         'library' => array(
+            'Base class uplifted to latest version',
+            'Html class submit method adjusted for multiple parameters',
+            'Main class added error handling method',
+            'Main class added player data handling methods',
+            'Input class updated variable acquistion method',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.0.2',
+      'datetime' => '2023-03-31 18:30 UTC',
+      'changes' => array(
+         'backend' => array(
+            'Adjustments to minimum speed assessment',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.0.1',
+      'datetime' => '2023-03-19 17:30 UTC',
+      'changes' => array(
+         'frontend' => array(
+            'Mobile rendering updates',
+            'Loaded favicon',
+            'Modified percentage bars in analytics to use progress-bar class',
+         ),
+      ),
+   ),
+   array(
+      'version' => '1.0.0',
+      'datetime' => '2023-03-18 03:30 UTC',
+      'changes' => array(
+         'system' => array(
+            'Initial build',
+         ),
+      ),
+   ),
 );
+
+$notesDisplay = '';
+
+foreach ($versionList as $versionPatch) {
+   $notesDisplay .= displayPatchNote($versionPatch);
+}
+
+print $notesDisplay;
+
+include 'ui/footer.php';
+
+?>
+<?php
+
+function displayPatchNote($patchInfo)
+{
+   $changeAttrib = array(
+      'system'   => array('label' => 'System', 'color' => 'text-primary'),
+      'frontend' => array('label' => 'Frontend', 'color' => 'text-green'),
+      'backend'  => array('label' => 'Backend', 'color' => 'text-red'),
+      'library'  => array('label' => 'Library', 'color' => 'text-pink'),
+   );
+
+   $patchVersion  = $patchInfo['version'];
+   $patchDatetime = $patchInfo['datetime'];
+
+   $return = "<div class='row'>\n".
+             "  <div class='col-12 col-xl-9 col-lg-10 col-md-12 col-sm-12'>\n".
+             "    <div class='card card-outline card-success'>\n".
+             "      <div class='card-header'>\n".
+             "        <b class='text-xl'>v$patchVersion</b><div class='card-tools text-yellow'>$patchDatetime</div>\n".
+             "      </div>\n".
+             "      <div class='card-body'>\n".
+             "        <ul>\n";
+
+   foreach ($patchInfo['changes'] as $changeType => $changeList) {
+      foreach ($changeList as $changeText) {
+         $changeLabel = $changeAttrib[$changeType]['label'];
+         $changeColor = $changeAttrib[$changeType]['color'];
+
+         $return .= "             <li><span class='$changeColor'>$changeLabel:</span> $changeText</li>\n"; 
+      }
+   }
+
+   $return .= "        </ul>\n".
+              "      </div>\n".
+              "    </div>\n".
+              "  </div>\n".
+              "</div>\n";
+
+   return $return;
+}
 
 ?>
 
+<!--
 <div class="row">
   <div class="col-12 col-xl-9 col-lg-10 col-md-12 col-sm-12">
     <div class="card card-outline card-success">
@@ -404,7 +776,7 @@ $versionList = array(
           <li><span class='text-green'>Frontend:</span> Runeposts area created</li>
           <li><span class='text-green'>Frontend:</span> Cleanup page layout inconsistencies</li>
           <li><span class='text-green'>Frontend:</span> Updated title dipslay on all pages</li>
-          <li><span class='text-primary'>Simulator:</span> Added Weapon and Shield Skin to system</li>
+          <li><span class='text-red'>Backend:</span> Added Weapon and Shield Skin to Simulator</li>
         </ul>
       </div>
     </div>
@@ -437,7 +809,7 @@ $versionList = array(
       <div class="card-body">
         <ul>
           <li><span class='text-green'>Frontend:</span> Updated title method for displaying page title</li>
-          <li><span class='text-primary'>Simulator:</span> Added DPS to output</li>
+          <li><span class='text-primary'>Backend:</span> Added DPS to output in Simulator</li>
           <li><span class='text-pink'>Library:</span> Base class uplifted to latest version</li>
           <li><span class='text-pink'>Library:</span> Html class submit method adjusted for multiple parameters</li>
           <li><span class='text-pink'>Library:</span> Main class added error handling method</li>
@@ -465,41 +837,4 @@ $versionList = array(
   </div>
 </div>
 
-
-<div class="row">
-  <div class="col-12 col-xl-9 col-lg-10 col-md-12 col-sm-12">
-    <div class="card card-outline card-success">
-      <div class="card-header">
-        <b class="text-xl">v1.0.1</b><div class="card-tools text-yellow">2023-03-19 17:30 UTC</div>
-      </div>
-      <div class="card-body">
-        <ul>
-          <li><span class='text-green'>Frontend:</span> Mobile rendering updates</li>
-          <li><span class='text-green'>Frontend:</span> Loaded favicon</li>
-          <li><span class='text-green'>Frontend:</span> Modified percentage bars in analytics to use progress-bar class</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="row">
-  <div class="col-12 col-xl-9 col-lg-10 col-md-12 col-sm-12">
-    <div class="card card-outline card-success">
-      <div class="card-header">
-        <b class="text-xl">v1.0.0</b><div class="card-tools text-yellow">2023-03-18 03:30 UTC</div>
-      </div>
-      <div class="card-body">
-        <ul>
-          <li>Initial build</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
-
-<?php
-
-include 'ui/footer.php';
-
-?>
+-->
