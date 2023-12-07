8f8eb69dd7bb102908cc024c5659ea2299afe95f
ryhoneyman <ryhoneyman@gmail.com>
2023-12-07 14:03:32 +0000

    removed variables

--- www/check.php
+++ www/check.php
@@ -4,6 +4,9 @@ $info = array(
    'SERVER' => $_SERVER,
 );
 
+unset($info['SERVER']['DOCUMENT_ROOT']);
+unset($info['SERVER']['SCRIPT_FILENAME']);
+
 print "<pre>\n";
 
 print json_encode($info,JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT)."\n";
